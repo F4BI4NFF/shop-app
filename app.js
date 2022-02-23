cdef5f1bda65c993cb44af3c7d24099cdff9022c
Fabián Andrés Fernández Figueroa <fabian.fernandez.13@sansano.usm.cl>
2022-02-23 18:44:09 -0300

    Fetching data from MySQL DB

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -15,6 +16,8 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
