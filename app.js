16ea4ccce1fac62711ceb7bb9cdd5d0562969db8
Fabián Andrés Fernández Figueroa <fabian.fernandez.13@sansano.usm.cl>
2022-02-24 21:16:17 -0300

    Using Sequelize (ORM third party package) to handle DB objects

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
+
 
 const app = express();
 
@@ -17,9 +24,19 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-
-
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
 
 app.use('/admin', adminRoutes);
 
@@ -27,4 +44,42 @@ app.use(shopRoutes);
 
 app.use(errorController.getError404);
 
-app.listen(3000);
+Product.belongsTo(User, {
+  constraints: true,
+  onDelete: 'CASCADE'
+});
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {
+  through: CartItem
+});
+Product.belongsToMany(Cart, {
+  through: CartItem
+});
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem});
+
+sequelize
+  .sync()
+  // .sync({force: true})
+  .then(result => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({
+        name: 'Fabian',
+        email: 'fabian@hotmail.com'
+      });
+    }
+    return user;
+  })
+  .then(user => {
+    return user.createCart();
+  })
+  .then(cart => {
+    app.listen(3000);
+  })
+  .catch(err => console.log(err));
